916094f2f5093f3257f5f821eec51e7cfd3ce177
Armin Mendieta <antonio9949257@gmail.com>
2023-02-25 20:51:09 -0400

    ordenando en los directorios

---
+++ task2/AreaCircunferencia.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    double R, PI=3.1416, A;
+    cout << "Introducir R: ";
+    cin >> R;
+    A = PI * R * R;
+    cout << "El area es: "<< A;
+    return 0;
+}
