1a9a982d3de1b0552150e87c1ac11be166eba64e
Armin Mendieta <antonio9949257@gmail.com>
2023-03-03 22:57:35 -0400

    agragando el ejercicio 4 del tercer laboratorio

---
+++ lab3/mayorABC.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+
+public class Main {
+  public static void main(String[] args) {
+    Scanner input = new Scanner(System.in);
+    int a, b, c;
+    System.out.print("Ingrese el primer número: ");
+    a = input.nextInt();
+    System.out.print("Ingrese el segundo número: ");
+    b = input.nextInt();
+    System.out.print("Ingrese el tercer número: ");
+    c = input.nextInt();
+  
+    if (a == b && b == c ){
+        System.out.println("Los tres numero ingresados son iguales: "+a+" "+b+" "+c)
+    }else if(a==b||a==b||a==b){
+        System.out.println("Hay dos numero igules: ");
+        if (a == b){
+            System.out.println(a+" y "+b)
+        } else if (a == c){
+            System.out.println(a + " y " + b)
+        } else {
+            System.out.println(b + " y " + c)
+        }
+    }else{
+        int mayor = a;
+        if ( b > mayor){
+            mayor = b;
+        }
+        if (c > mayor){
+            mayor = c
+        }
+        System.out.println("El numero mayor es: "+ mayor);
+    }
+  }
+}
